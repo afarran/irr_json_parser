b7ba2a43a0f0cafd18ff3ea3f527cfb07fe90f14
Amjad Farran <amjad.farran@yahoo.ca>
2022-03-11 13:22:09 -0500

    Initial implementation of the IRR JSON to CSV converter.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # irr_json_parser
-Tool that converts IRR entries in JSON format to CSV and flags IRR entries that have advertised routes and are ARIN-NONAUTH only 
+
+Tool that converts IRR entries in JSON format to CSV and flags IRR entries that have advertised routes and are `ARIN-NONAUTH` only.
+This can help identify routes in that are still in the ARIN NONAUTH IRR, which was announced to [be retired in April 2022](https://www.arin.net/announcements/20220225-irr). These routes should be migrated to ARIN AUTH IRR or other IRR to avoid potential outages.
+
+## Usage
+
+The input to the tool is an IRR file in JSON format (e.g. [irr_example.json](irr_example.json) and the output is a CSV file with the same file name and file path (e.g. [irr_example.csv](irr_example.csv)). The output CSV file has a boolean field that would show `TRUE` for advertised routes that exist only in `ARIN-NONAUTH` IRR.
+
+```PowerShell
+>lua.exe irr_json_parser.lua <JSON source file name>
+```
---
+++ irr_json_parser.lua
@@ -0,0 +1,46 @@
+local json = loadfile('json.lua\\json.lua')()
+
+local inputJsonFile = arg[1] -- "IRR_EXAMPLE.json"
+if not inputJsonFile then
+  print("Usage:\n >lua.exe irr_json_parser.lua <JSON source file name>")
+  return
+end
+
+local outputCsvFile = inputJsonFile:lower():gsub("\\.json", "\\.csv")
+print("Reading from ", inputJsonFile)
+local fileHandle, err = io.open(inputJsonFile, "r")
+if fileHandle then
+  local jsonText = fileHandle:read("*a"):gsub("%c", "") --remove control characters
+  fileHandle:close()
+  local jsonTable = json.decode(jsonText)
+  print("Writing to ", outputCsvFile)
+  fileHandle = io.open(outputCsvFile, "w")
+  if fileHandle then
+    local totalRecords = 0
+    local totalAdverstisedArinNonAuthOnlyRecords = 0
+    local str = "IsAdvertisedAndArinNonAuthOnly,Index,rir,bgpOrigins,prefix,prefixSortKey,goodnessOverall,routeSource,asn,rpslPk,rpkiStatus,rpkiMaxLength,routeSource,asn,rpslPk,rpkiStatus,rpkiMaxLength,routeSource,asn,rpslPk,rpkiStatus,rpkiMaxLength"
+    fileHandle:write(str.."\n")
+    for idx, val in ipairs(jsonTable) do
+      totalRecords = totalRecords + 1
+      str = string.format("%s,%s,%s,%s,%s,%s", idx, val.rir or "", val.bgpOrigins[1] or "", val.prefix or "", val.prefixSortKey or "", val.goodnessOverall or "")
+      local isArinNonAuthOnly
+      for key, routeVal in pairs(val.irrRoutes) do
+        if val.bgpOrigins[1] and key ~= "ARIN-NONAUTH" then
+          isArinNonAuthOnly = false
+        elseif val.bgpOrigins[1] and isArinNonAuthOnly == nil and key == "ARIN-NONAUTH" then
+          isArinNonAuthOnly = true
+          totalAdverstisedArinNonAuthOnlyRecords = totalAdverstisedArinNonAuthOnlyRecords + 1
+        end
+        local routeTable = routeVal[1]
+        str = str..string.format(",%s,%s,%s,%s,%s", key, routeTable.asn, routeTable.rpslPk, routeTable.rpkiStatus, routeTable.rpkiMaxLength or "")
+      end
+      fileHandle:write(tostring(isArinNonAuthOnly)..","..str.."\n")
+    end
+    fileHandle:close()
+    print(string.format("Found %d total records in %s. %d are advertised ARIN-NONAUTH routes. Output is stored in %s.", totalRecords, inputJsonFile, totalAdverstisedArinNonAuthOnlyRecords, outputCsvFile))
+  else
+    print("File writing error: ", err)
+  end
+else
+  print("File reading error: ", err)
+end
